a013b21f402138deb36bc09ba4115f9789ff7024
Souhayl Aguedai <aguedaisouhayl@gmail.com>
2026-02-10 15:06:28 +0100

    Initialize Spring Boot backend with core dependencies

---
+++ backend/src/main/java/com/souhayl/bball/BasketballTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.souhayl.bball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballTrainingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/souhayl/bball/BasketballTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souhayl.bball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
